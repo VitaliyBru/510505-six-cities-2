82f982dff817f8394ef336afef586347c3fdb65c
Vitaly Bryukhovetsky <bruhovet@list.ru>
2019-11-04 01:25:51 +0300

    Добавляет module2-task2

--- src/index.js
+++ src/index.js
@@ -2,13 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {App} from "./components/app/app.jsx";
 
+
+const cardNameList = [
+  {
+    name: `Beautiful &amp; luxurious apartment at great location`,
+    id: 0
+  },
+  {
+    name: `Wood and stone place`,
+    id: 1
+  },
+  {
+    name: `Canal View Prinsengracht`,
+    id: 2
+  },
+  {
+    name: `Nice, cozy, warm big bed apartment`,
+    id: 3
+  },
+];
+
 const init = () => {
-  const cardNameList = [
-    `Beautiful &amp; luxurious apartment at great location`,
-    `Wood and stone place`,
-    `Canal View Prinsengracht`,
-    `Nice, cozy, warm big bed apartment`,
-  ];
   ReactDOM.render(
       <App cardNamesList={cardNameList}/>,
       document.getElementById(`root`)
